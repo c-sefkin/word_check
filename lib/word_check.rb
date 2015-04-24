a911e7354178bdb1882c6c993d599ad22683bf85
Chris Sefkin <c.sefkin@gmail.com>
2015-04-24 09:47:48 -0700

    specs working! now case independent

--- lib/word_check.rb
+++ lib/word_check.rb
@@ -1,7 +1,9 @@
 class String
   define_method(:word_check) do |sentence|
-    original_sentence = sentence.split()
-    result = original_sentence.count(self)
+    word = self.downcase()
+    lower_sentence = sentence.downcase()
+    original_sentence = lower_sentence.split()
+    result = original_sentence.count(word)
     result
   end
 end
--- spec/word_check_spec.rb
+++ spec/word_check_spec.rb
@@ -8,4 +8,7 @@ describe('String#word_check') do
   it("returns how many times a specified word is represented in a sentence") do
     expect(("coding").word_check("i love coding coding coding")).to(eq(3))
   end
+  it("returns how many times a specified word is represented in a sentence") do
+    expect(("Coding").word_check("i love coding coding coding.")).to(eq(3))
+  end
 end
